8ae20f980aa510e46d892c68c8fae52aa395702f
aryanp-07 <aryanpatnaik07@gmail.com>
2023-11-13 21:45:39 +0530

    set host

--- app.py
+++ app.py
@@ -284,5 +284,5 @@ def get_suggestions():
 
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=5000, threaded=True)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, threaded=True)
+    # app.run(debug=True)
