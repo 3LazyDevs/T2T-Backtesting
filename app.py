a665d6c50d10c08b7e1f10311a1b44518c57d165
aryanp-07 <aryanpatnaik07@gmail.com>
2023-11-14 18:01:28 +0530

    Added host

--- app.py
+++ app.py
@@ -363,5 +363,5 @@ def delete_file(filename):
 
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=5000, threaded=True)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, threaded=True)
+    # app.run(debug=True)
