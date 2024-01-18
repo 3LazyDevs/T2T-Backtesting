f782464d53b87cb5fcab024126c54dba03e1cbe7
aryanp-07 <aryanpatnaik07@gmail.com>
2024-01-18 15:33:34 +0530

    updated app.py

--- app.py
+++ app.py
@@ -441,5 +441,5 @@ def delete_file(filename):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, threaded=True)
+    app.run(host="0.0.0.0", port=5000, threaded=True)
     # app.run(debug=True)
