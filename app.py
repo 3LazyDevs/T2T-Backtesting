6007b96963fe23aeba0f12cfae966d08ea0c4353
aryanp-07 <aryanpatnaik07@gmail.com>
2025-01-13 16:19:31 +0530

    utf8 fix

--- app.py
+++ app.py
@@ -415,4 +415,5 @@ def delete_file(filename):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, threaded=True)
+    # app.run(host="127.0.0.1", port=5000, threaded=True)
     # app.run(debug=True)
